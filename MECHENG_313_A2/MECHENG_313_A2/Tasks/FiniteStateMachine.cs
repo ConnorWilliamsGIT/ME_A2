b78e6f9f943d15733138de186645563ff1f7d63e
Connor Williams <conjob.2010@gmail.com>
2023-05-24 14:21:39 +1200

    recreated project files

---
+++ MECHENG_313_A2/MECHENG_313_A2/Tasks/FiniteStateMachine.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MECHENG_313_A2.Tasks
+{
+    public class FiniteStateMachine : IFiniteStateMachine
+    {
+        //table stores the next action and delegate to the action method to take during transition
+        private Dictionary<string, Dictionary<string, (TimestampedAction action, string nextState)>> fsTable;
+
+        private string currentState;
+        //define constructor
+        public FiniteStateMachine() // string startingState
+        {
+            //set current state and create the table
+            // currentState = startingState;
+            fsTable = new Dictionary<string, Dictionary<string, (TimestampedAction action, string nextState)>>();
+        }
+
+        public void addState(string state)
+        {  
+            //check if the state already exists
+            if (fsTable.ContainsKey(state) == false)
+            {
+                //if not create the new state with a blank dictionary of actions
+                fsTable.Add(state, new Dictionary<string, (TimestampedAction action, string nextState)>());
+            }
+            else
+            {
+                //todo log that the state already exists
+            }
+
+        }
+        
+        public void AddAction(string state, string eventTrigger, TimestampedAction action)
+        {   
+            //check if the state exists
+            if (fsTable.ContainsKey(state) == false)
+            {
+                //todo log that the state doesnt exist
+                return;
+            }
+            //check if the action already exists on the state
+            if (fsTable[state].ContainsKey(eventTrigger))
+            {
+                //if it does then add the action and keep the current "next state"
+                fsTable[state][eventTrigger] = (action, fsTable[state][eventTrigger].nextState);
+            }
+            else
+            {
+                //else create the new action with a null next state
+                fsTable[state].Add(eventTrigger, (action, null));
+                
+            }
+            
+        }
+
+        public void createEvent(string state, string eventTrigger, TimestampedAction action, string nextState)
+        {
+            //check if the state exists
+            if (fsTable.ContainsKey(state) == false)
+            {
+                //todo log that the state doesnt exist
+                return;
+            }
+            //check if the action already exists on the state
+            if (fsTable[state].ContainsKey(eventTrigger))
+            {
+                //if it does then add the action and keep the current "next state"
+                fsTable[state][eventTrigger] = (action, nextState);
+            }
+            else
+            {
+                //else create the new action with a null next state
+                fsTable[state].Add(eventTrigger, (action, nextState));
+                
+            }
+        }
+
+        public string GetCurrentState()
+        {
+            return currentState;
+        }
+
+        public string ProcessEvent(string eventTrigger)
+        {
+            if (!fsTable[currentState].ContainsKey(eventTrigger)) return currentState;
+            if (fsTable[currentState][eventTrigger].nextState == null) return currentState;
+            string current = currentState;
+            //start the thread to run the action
+            Thread actionThread = new Thread(new ThreadStart(() =>
+            {
+                lock (current)
+                {
+                    fsTable[current][eventTrigger].action(DateTime.Now);    
+                }
+            }));
+            actionThread.Start();
+            currentState = fsTable[currentState][eventTrigger].nextState ?? currentState;
+            //return the next state if it exists or the current state if it doesnt
+            return currentState;
+        }
+
+        public void SetCurrentState(string state)
+        {
+            currentState = state;
+        }
+
+        public void SetNextState(string state, string nextState, string eventTrigger)
+        {
+            //check if the state exists
+            if (fsTable.ContainsKey(state) == false)
+            {
+                //todo log that the state doesnt exist
+                return;
+            }
+            if (fsTable[state].ContainsKey(eventTrigger))
+            {
+                //if it does then add the action and keep the current "next state"
+                fsTable[state][eventTrigger] = (fsTable[state][eventTrigger].action, nextState);
+            }
+            else
+            {
+                //else create the new action with a null next state
+                fsTable[state].Add(eventTrigger, (null, nextState));
+            }
+        }
+    }
+}
---
+++ MECHENG_313_A2/MECHENG_313_A2/Tasks/Task2.cs
@@ -0,0 +1,145 @@
+﻿using MECHENG_313_A2.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using MECHENG_313_A2.Serial;
+
+namespace MECHENG_313_A2.Tasks
+{
+    
+    
+    internal class Task2 : IController
+    {
+        
+        
+        private MockSerialInterface serialInterface = new MockSerialInterface();
+        private List<string> states = new List<string> {"red", "green", "yellow", "configYellow", "configBlack"};
+        private List<string> events = new List<string> {"tick", "config", "tick-c"};
+        private bool configRequested = false;
+        private FiniteStateMachine fsm = new FiniteStateMachine();
+        
+        public Task2()
+        {
+            foreach (string state in states)
+            {
+                fsm.addState(state);
+            }
+
+            fsm.createEvent(states[0], events[0], GoToGreen, states[1]); // Tick : red to green
+            fsm.createEvent(states[1], events[0], GoToYellow, states[2]); // Tick :green to yellow
+            fsm.createEvent(states[2], events[0], GoToRed, states[0]); // Tick :yellow to red
+            fsm.createEvent(states[3], events[0], GoToBlack, states[4]); // Tick : configYellow to configBlack
+            fsm.createEvent(states[4], events[0], GoToYellow, states[3]); // Tick : configBlack to configYellow
+            fsm.createEvent(states[0], events[1], GoToYellow, states[3]); // Config : red to configYellow
+            fsm.createEvent(states[3], events[1], GoToRed, states[0]); // Config : configYellow to red
+            fsm.createEvent(states[4], events[1], GoToRed, states[0]); // Config : configBlack to red
+            fsm.createEvent(states[0], events[2], GoToYellow, states[3]); // Tick-c : red to configYellow
+            fsm.createEvent(states[1], events[2], GoToYellow, states[2]); // Tick-c : green to yellow
+            fsm.createEvent(states[2], events[2], GoToRed, states[0]); // Tick-c : yellow to red
+        }
+
+        public virtual TaskNumber TaskNumber => TaskNumber.Task2;
+
+        protected ITaskPage _taskPage;
+
+        private void GoToGreen(DateTime timeStamp)
+        {
+            _taskPage.SetTrafficLightState(TrafficLightState.Green);
+        }
+        private void GoToRed(DateTime timeStamp)
+        {
+            _taskPage.SetTrafficLightState(TrafficLightState.Red);
+        }
+
+        private void GoToYellow(DateTime timeStamp)
+        {
+            _taskPage.SetTrafficLightState(TrafficLightState.Yellow);
+        }
+
+        private void GoToBlack(DateTime timeStamp)
+        {
+            _taskPage.SetTrafficLightState(TrafficLightState.None);
+        }
+        
+        public void ConfigLightLength(int redLength, int greenLength)
+        {
+            // TODO: Implement this
+        }
+
+        public async Task<bool> EnterConfigMode()
+        {
+            try
+            { 
+                configRequested = fsm.ProcessEvent(events[1]) != states[3];
+            }
+            catch (Exception e)
+            {
+                // todo log error
+                return false;
+            }
+
+            return true;
+        }
+
+        public void ExitConfigMode()
+        {
+            configRequested = false;
+            if (fsm.GetCurrentState() == states[0])
+            {
+                return;
+            }
+            fsm.ProcessEvent(events[1]);
+        }
+
+        public async Task<string[]> GetPortNames()
+        {
+            return await serialInterface.GetPortNames();
+        }
+
+        public async Task<string> OpenLogFile()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "log.txt");
+            // if(!File.Exists(filePath))
+            // {
+            //     File.Create(filePath);
+            // }
+            // string text = File.ReadAllText(filePath);
+            // return text ?? "log file empty";
+        }
+
+        public async Task<bool> OpenPort(string serialPort, int baudRate)
+        {
+            try
+            {
+                serialInterface.OpenPort(serialPort, baudRate);
+            }
+            catch (Exception e)
+            {
+                //todo add log for this
+                return false;
+            }
+            return true;
+        }
+
+        public void RegisterTaskPage(ITaskPage taskPage)
+        {
+            _taskPage = taskPage;
+        }
+
+        public void Start()
+        {
+            fsm.SetCurrentState(states[1]);
+            _taskPage.SetTrafficLightState(TrafficLightState.Green);
+        }
+
+        public void Tick()
+        {
+            if (fsm.ProcessEvent(configRequested ? "tick-c" : "tick") == states[3])
+            {
+                configRequested = false;
+            };
+        }
+    }
+}
